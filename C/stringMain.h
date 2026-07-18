33efd5ce66012d7063fb1ff237789677892f4cb1
Charlotte Machan <charlottemachan@macbookair.home>
2026-07-17 22:56:23 -0400

    header for c functions

---
+++ C/stringMain.h
@@ -0,0 +1,12 @@
+#ifndef STRINGMAIN_H
+#define STRINGMAIN_H
+
+int countChars (const char *str);
+int countWords (const char *str);
+void reverseString (char *str);
+void reverseWords (char *str);
+int countVowels (const char *str);
+void upperCase (char *str);
+void lowerCase (char *str);
+
+#endif
